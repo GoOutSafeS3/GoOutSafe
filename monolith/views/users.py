3f1923bfbc210c7a9111d7144724cc3f9f55b334
Monica Amico <Monicat@Air-di-Monica.lan>
2020-10-23 20:28:47 +0200

    add make_response to return HTTP response code

--- monolith/views/users.py
+++ monolith/views/users.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, redirect, render_template, request, flash
+from flask import Blueprint, redirect, render_template, request, flash, make_response
 from monolith.auth import admin_required
 from monolith.forms import UserForm
-from monolith.database import  User, db
+from monolith.database import User, db
 
 users = Blueprint('users', __name__)
 
@@ -12,7 +12,6 @@ def _users():
     return render_template("users.html", users=users)
 
 
-@admin_required
 @users.route('/create_user', methods=['GET', 'POST'])
 def create_user():
     form = UserForm()
@@ -23,7 +22,7 @@ def create_user():
             password_repeat = request.form['password_repeat']
             if password != password_repeat:
                 flash('Le password non coincidono', 'warning')
-                return redirect('#')
+                return make_response(create_user(),400)
 
             userGet = User.query.filter_by(email=form.email.data).first()
             if userGet is None:
@@ -35,12 +34,12 @@ def create_user():
                     db.session.commit()
                 except:
                     flash('Utente non inserito','error')
-                    return redirect('#')
+                    return make_response(create_user(),500)
             else:
                 flash('Utente esistente', 'error')
-                return redirect('#')
+                return make_response(create_user(),400)
 
             flash('Utente registrato con successo','success')
-            return redirect('/users')
+            return make_response(_users(),200)
 
     return render_template('create_user.html', form=form)
