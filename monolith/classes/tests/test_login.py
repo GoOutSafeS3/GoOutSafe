bf4862238fa04788d7e383a11f2fd1dad1a334aa
marco <marco@DESKTOP-JJ6QP2E.localdomain>
2020-10-23 14:44:58 +0200

    formatted

--- monolith/classes/tests/test_login.py
+++ monolith/classes/tests/test_login.py
@@ -5,7 +5,6 @@ app = create_app()
 
 app.config['TESTING'] = True
 app.config['WTF_CSRF_ENABLED'] = False
-#app.config['SERVER_NAME'] = False
 
 class TestLogin(unittest.TestCase):
 
@@ -15,8 +14,6 @@ class TestLogin(unittest.TestCase):
         self.assertTrue("form" in reply.get_data(as_text=True))
 
     def test_login_post_admin(self):
-        TESTING = True
-        WTF_CSRF_ENABLED = False
         tested_app = app.test_client()
         reply = tested_app.post('/login',data={"email":"example@example.com", "password": "admin"})
         with app.test_request_context():
