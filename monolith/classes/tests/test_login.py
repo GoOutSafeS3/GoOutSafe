4566b0b031daa7a3e170d9f7ad6ec2e505624ccf
marco <marco@DESKTOP-JJ6QP2E.localdomain>
2020-10-24 11:22:37 +0200

    added test and code for error handling

--- monolith/classes/tests/test_login.py
+++ monolith/classes/tests/test_login.py
@@ -11,10 +11,22 @@ class TestLogin(unittest.TestCase):
     def test_login_get_1(self):
         tested_app = app.test_client()
         reply = tested_app.get('/login')
+        self.assertEqual(reply.status_code, 200)
         self.assertTrue("form" in reply.get_data(as_text=True))
 
     def test_login_post_admin(self):
         tested_app = app.test_client()
         reply = tested_app.post('/login',data={"email":"example@example.com", "password": "admin"})
+        self.assertEqual(reply.status_code, 302)
         with app.test_request_context():
             self.assertEqual(reply.location, url_for('home.index',_external=True))
+
+    def test_login_post_wrong(self):
+        tested_app = app.test_client()
+        reply = tested_app.post('/login',data={"email":"example@example.com", "password": "wrong"})
+        self.assertEqual(reply.status_code, 401)
+
+    def test_login_post_bad(self):
+        tested_app = app.test_client()
+        reply = tested_app.post('/login',data={"email":"example@example.com", "password": None})
+        self.assertEqual(reply.status_code, 400)
