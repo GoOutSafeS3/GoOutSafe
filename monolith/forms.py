052849a27f22eab1b5b5c63adfa6c997e1e236ca
GBisi <giuseppe.bisicchia@outlook.it>
2020-10-24 15:39:36 +0200

    Added operator test

--- monolith/forms.py
+++ monolith/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 import wtforms as f
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, InputRequired
 from wtforms.fields.html5 import EmailField
 
 
@@ -11,13 +11,13 @@ class LoginForm(FlaskForm):
 
 
 class UserForm(FlaskForm):
-    email = EmailField('email', validators=[DataRequired(), Email()])
-    firstname = f.StringField('firstname', validators=[DataRequired()])
-    lastname = f.StringField('lastname', validators=[DataRequired()])
-    password = f.PasswordField('password', validators=[DataRequired()])
-    password_repeat = f.PasswordField('repeat password', validators=[DataRequired()])
-    telephone = f.IntegerField('telephone', validators=[DataRequired()]) #GB add validator 
-    dateofbirth = f.DateField('dateofbirth', format='%d/%m/%Y')
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    firstname = f.StringField('Firstname', validators=[DataRequired()])
+    lastname = f.StringField('Lastname', validators=[DataRequired()])
+    password = f.PasswordField('Password', validators=[DataRequired()])
+    password_repeat = f.PasswordField('Repeat Password', validators=[DataRequired()])
+    telephone = f.IntegerField('Telephone', validators=[InputRequired()]) 
+    dateofbirth = f.DateField('Date Of Birth', format='%d/%m/%Y')
 
     display = ['email', 'firstname', 'lastname', 'password','password_repeat', 'telephone', 'dateofbirth']
 
@@ -27,13 +27,13 @@ class OperatorForm(FlaskForm):
     firstname = f.StringField('Firstname', validators=[DataRequired()])
     lastname = f.StringField('Lastname', validators=[DataRequired()])
     password = f.PasswordField('Password', validators=[DataRequired()])
-    password_repeat = f.PasswordField('Repeat password', validators=[DataRequired()])
-    telephone = f.StringField('Telephone', validators=[DataRequired()]) #GB add validator
+    password_repeat = f.PasswordField('Repeat Password', validators=[DataRequired()])
+    telephone = f.IntegerField('Telephone', validators=[InputRequired()]) #GB add validator
     dateofbirth = f.DateField('Date Of Birth', format='%d/%m/%Y')
 
     restaurant_name = f.StringField('Restaurant Name',validators=[DataRequired()])
-    restaurant_phone = f.IntegerField('Restaurant Phone', validators=[DataRequired()])
-    restaurant_latitude = f.FloatField('Restaurant latitude', validators=[DataRequired()])
-    restaurant_longitude = f.FloatField('Restaurant Longitude', validators=[DataRequired()])
+    restaurant_phone = f.IntegerField('Restaurant Phone', validators=[InputRequired()])
+    restaurant_latitude = f.FloatField('Restaurant latitude', validators=[InputRequired()])
+    restaurant_longitude = f.FloatField('Restaurant Longitude', validators=[InputRequired()])
 
     display = ['email', 'firstname', 'lastname', 'password','password_repeat', 'telephone', 'dateofbirth','restaurant_name','restaurant_phone', 'restaurant_latitude', 'restaurant_longitude']
