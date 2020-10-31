4b8077514903c9a674659eeed585f9a2a02daab3
GBisi <giuseppe.bisicchia@outlook.it>
2020-10-31 10:01:07 +0100

    Updated run.sh with build

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export FLASK_APP="monolith/app.py:create_app"
+docker-compose build
+
+if [[ "$1" == "local" ]]; then 
+    docker-compose up local &
+    flask run
+elif [[ "$1" == "local-tests" ]]; then 
+    docker-compose up local &
+    pytest --cov=monolith --cov-report term-missing --cov-report html --html=report.html
+elif [[ "$1" == "tests" ]]; then 
+    docker-compose up tests
+elif [[ "$1" == "monolith" ]]; then 
+    docker-compose up monolith
+else
+    echo "Service not recognized"
+fi;
