b1a5c9322c565e528ef3dcb55a536482f596cd0e
GBisi <giuseppe.bisicchia@outlook.it>
2020-11-04 20:47:01 +0100

    Added some comments

--- monolith/forms.py
+++ monolith/forms.py
@@ -5,6 +5,9 @@ from wtforms.fields.html5 import EmailField
 
 
 class LoginForm(FlaskForm):
+    """
+    Form to login
+    """
     email = f.StringField('Email', validators=[DataRequired()])
     password = f.PasswordField('Password', validators=[DataRequired()])
 
@@ -12,6 +15,9 @@ class LoginForm(FlaskForm):
 
 
 class UserForm(FlaskForm):
+    """
+    Form to insert a user's data (profile)
+    """
     email = EmailField('Email', validators=[DataRequired(), Email()])
     firstname = f.StringField('Firstname', validators=[DataRequired()])
     lastname = f.StringField('Lastname', validators=[DataRequired()])
@@ -25,6 +31,9 @@ class UserForm(FlaskForm):
 
 
 class EditUserForm(FlaskForm):
+    """
+    Form to edit a user's data (profile)
+    """
     firstname = f.StringField('Firstname')
     lastname = f.StringField('Lastname')
     new_password = f.PasswordField('Password')
@@ -37,6 +46,9 @@ class EditUserForm(FlaskForm):
 
 
 class OperatorForm(FlaskForm):
+    """
+    Form to insert a operator's data (profile)
+    """
     email = EmailField('Email', validators=[DataRequired(), Email()])
     firstname = f.StringField('Firstname', validators=[DataRequired()])
     lastname = f.StringField('Lastname', validators=[DataRequired()])
@@ -54,6 +66,9 @@ class OperatorForm(FlaskForm):
 
 
 class RestaurantEditForm(FlaskForm):
+    """
+    Form to insert/edit a restaurant
+    """
     name = f.StringField('Restaurant Name',validators=[DataRequired()])
     phone = f.StringField('Restaurant Phone', validators=[DataRequired()])
     lat = f.FloatField('Restaurant Latitude', validators=[InputRequired()])
@@ -71,14 +86,23 @@ class RestaurantEditForm(FlaskForm):
 
 
 class TableAddForm(FlaskForm):
+    """
+    Form to insert/edit a table in a restaurant
+    """
     capacity = f.IntegerField('Table capacity', validators=[InputRequired()])
     display = ['capacity']
 
 class RatingAddForm(FlaskForm):
+    """
+    Form to add a rating in a restaurant
+    """
     rating = f.IntegerField('Restaurant rating', validators=[InputRequired()])
     display = ['rating']
 
 class BookingForm(FlaskForm):
+    """
+    Form to insert/edit a reservation
+    """
     number_of_people = f.IntegerField("Number of people", validators=[InputRequired(), NumberRange(min=1)])
     booking_date = f.DateField('Date', format='%d/%m/%Y')
     booking_hr = f.SelectField('Hour', choices=range(0,24))
@@ -88,6 +112,9 @@ class BookingForm(FlaskForm):
 
 
 class BookingList(FlaskForm):
+    """
+    Form to find all the reservations in a specific period
+    """
     from_date = f.DateField('From Date', format='%d/%m/%Y')
     from_hr = f.SelectField('Hour', choices=range(0,24))
     from_min = f.SelectField('Minutes', choices=range(0,60,15))
@@ -99,6 +126,9 @@ class BookingList(FlaskForm):
 
 
 class SearchUserForm(FlaskForm):
+    """
+    Form to find a user by his/her identifiers
+    """
     email = f.StringField('Email')
     telephone = f.StringField('Telephone')
     ssn = f.StringField('SSN')
@@ -107,6 +137,9 @@ class SearchUserForm(FlaskForm):
 
 
 class SearchRestaurantForm(FlaskForm):
+    """
+    Form to find all the restaurants that respect certain parameters expressed in the filters
+    """
     name = f.StringField('Restaurant Name')
     opening_time = f.SelectField('Open at', choices=["Not Specified"]+([x for x in range(24)]))
     open_day = f.SelectField('Open on',  choices=[("0","Not Specified"), ("1","Monday"), ("2","Tuesday"), ("3","Wednesday"), ("4","Thursday"),("5","Friday"), ("6","Saturday"),("7","Sunday")])
