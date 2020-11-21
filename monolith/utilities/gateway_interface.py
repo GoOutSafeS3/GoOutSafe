5ad2a7cea7369c0df55c937dd9a54133f00e22ed
Francesco Bertolaccini <francesco@bertolaccini.dev>
2020-11-21 20:49:53 +0100

    Port contact_tracing to gateway

--- monolith/utilities/gateway_interface.py
+++ monolith/utilities/gateway_interface.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from werkzeug.security import check_password_hash, generate_password_hash
 from typing import Tuple, Dict, List
+from datetime import datetime
 
 class GatewayInterface(ABC):
     #### USER ####
@@ -11,12 +12,45 @@ class GatewayInterface(ABC):
 
     @classmethod
     @abstractmethod
+    def search_users(self, data: Dict[str, str]) -> Tuple[List[Dict], int]:
+        """
+        data fields:
+        - email: string
+        - telephone: string
+        - ssn: string
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
     def get_users(self) -> Tuple[List[Dict], int]:
         pass
 
     @classmethod
     @abstractmethod
     def _create_user(self, userdata: Dict) -> Tuple[Dict, int]:
+        pass
+
+    #### CONTACT TRACING ####
+
+    @classmethod
+    @abstractmethod
+    def get_positive_users(self) -> Tuple[List[Dict], int]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_user_contacts(self, user_id: int, begin: datetime, end: datetime) -> Tuple[List[Dict], int]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def mark_user(self, user_id: int) -> Tuple[Dict, int]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def unmark_user(self, user_id: int) -> Tuple[Dict, int]:
         pass
 
     @classmethod
--- monolith/views/contact_tracing.py
+++ monolith/views/contact_tracing.py
@@ -1,12 +1,9 @@
-from flask import Blueprint, redirect, render_template, request, flash, make_response, current_app
-from flask_login import login_required
-from monolith.auth import admin_required, health_authority_required
-from monolith.forms import UserForm, OperatorForm, SearchUserForm
-from monolith.database import User, db, Restaurant, Notification
-from monolith.utilities.contact_tracing import mark_as_positive, unmark_as_positive, get_user_contacts, get_operators_contacts
+from flask import Blueprint, redirect, render_template, request, flash, make_response
+from monolith.auth import health_authority_required
+from monolith.forms import SearchUserForm
 import datetime
 from datetime import timedelta, datetime
-from monolith.utilities.notification import add_notification, add_bookings_notifications
+from monolith.app import gateway
 
 contact_tracing = Blueprint('contact_tracing', __name__)
 
@@ -16,15 +13,14 @@ All features are only available to the health authority
 Error status codes:
     401 -- The user is not the health authority
 """
-
-
 @contact_tracing.route('/positives', methods=['GET'])
 @health_authority_required
 def positives():
     """ Allows you to view the list of currently positive users """
-    qry = db.session.query(User).filter_by(is_positive = True).all()
-    return render_template("positives.html", positives = qry, title="Positives")
-
+    positives, status = gateway.get_positive_users()
+    if positives is None or status != 200:
+        return make_response(render_template("error.html", error = status), status)
+    return render_template("positives.html", positives = positives, title="Positives")
 
 @contact_tracing.route('/positives/contacts', methods=['GET','POST'])
 @health_authority_required
@@ -33,45 +29,34 @@ def _contacts():
     form = SearchUserForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            qry = db.session.query(User)\
-            .filter_by(is_admin = False)\
-            .filter_by(is_health_authority=False)\
-            .filter_by(rest_id = None) # not an operator
-
             try:
                 if request.form["email"] == "" and request.form["telephone"] == "" and request.form["ssn"] == "":
                     flash("Please fill in a field","warning")
                     return render_template('form.html', form=form, title="Find Contacts")
 
-                if request.form["email"] != "":
-                    qry = qry.filter_by(email = request.form["email"])
-                if request.form["telephone"] != "":
-                    qry = qry.filter_by(phone = request.form["telephone"])
-                if request.form["ssn"] != "":
-                    qry = qry.filter_by(ssn = request.form["ssn"])
+                users, status = gateway.search_users(request.form)
             except:
                 flash("Bad Form","error")
                 return render_template('form.html', form=form, title="Find Contacts")
 
+            if users is None or status != 200:
+                return make_response(render_template('error.html', error = status), status)
 
-            qry = qry.all()
-
-            if len(qry) == 0:
+            if len(users) == 0:
                 flash("User not found","error")
                 return render_template('form.html', form=form, title="Find Contacts")
-            elif len(qry) > 1:
+            elif len(status) > 1:
                 flash("More users have been found, please try again","error")
                 return render_template('form.html', form=form, title="Find Contacts")
             else:
-                qry = qry[0]
+                user = users[0]
 
-            if qry.is_positive:
-                return redirect(f"/positives/{qry.id}/contacts")
+            if user['is_positive']:
+                return redirect(f"/positives/{user['id']}/contacts")
             else:
                 flash("The user is not positive","error")
                 
     return render_template('form.html', form=form, title="Find Contacts")
-
 
 @contact_tracing.route('/positives/mark', methods=['GET','POST'])
 @health_authority_required
@@ -85,49 +70,32 @@ def _mark_as_positive():
     form = SearchUserForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            qry = db.session.query(User)\
-            .filter_by(is_admin = False)\
-            .filter_by(is_health_authority=False)\
-            .filter_by(rest_id = None) # not an operator
-
             try:
                 if request.form["email"] == "" and request.form["telephone"] == "" and request.form["ssn"] == "":
                     flash("Please fill in a field","warning")
                     return render_template('mark_positives.html', form=form, title="Mark a User")
 
-                if request.form["email"] != "":
-                    qry = qry.filter_by(email = request.form["email"])
-                if request.form["telephone"] != "":
-                    qry = qry.filter_by(phone = request.form["telephone"])
-                if request.form["ssn"] != "":
-                    qry = qry.filter_by(ssn = request.form["ssn"])
+                users, status = gateway.search_users(request.form)
             except:
                 flash("Bad Form","error")
                 return render_template('mark_positives.html', form=form, title="Mark a User")
 
-            qry = qry.all()
+            if users is None or status != 200:
+                return make_response(render_template('error.html', error = status), status)
 
-            if len(qry) == 0:
+            if len(users) == 0:
                 flash("User not found","error")
                 return render_template('mark_positives.html', form=form, title="Mark a User")
-            elif len(qry) > 1:
+            elif len(status) > 1:
                 flash("More users have been found, please try again","error")
                 return render_template('mark_positives.html', form=form, title="Mark a User")
             else:
-                qry = qry[0]
+                user = users[0]
 
-            if mark_as_positive(qry.id):
+            result, status = gateway.mark_user(user['id'])
+
+            if result is not None and status == 200:
                 flash("The user was marked","success")
-                two_weeks = timedelta(days=14)
-                today = datetime.today()
-                date_start = today - two_weeks
-                users_to_be_notified = get_user_contacts(qry.id, date_start, today)
-                operators_to_be_notified = get_operators_contacts(qry.id, date_start, today)
-                for user in users_to_be_notified:
-                    add_notification(qry.id, user.id, 0) # notifications to other customers 
-                for operator in operators_to_be_notified:
-                    add_notification(qry.id, operator.id, 1) # notifications to operators
-                add_bookings_notifications(qry.id) # notifications for his/her bookings
                 return redirect("/positives")
             else:
                 flash("User not found","error")
@@ -143,42 +111,35 @@ def _unmark_as_positive():
     form = SearchUserForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            qry = db.session.query(User)\
-            .filter_by(is_admin = False)\
-            .filter_by(is_health_authority=False)\
-            .filter_by(rest_id = None) # not an operator
-            
             try:
                 if request.form["email"] == "" and request.form["telephone"] == "" and request.form["ssn"] == "":
                     flash("Please fill in a field","warning")
                     return render_template('form.html', form=form, title="Unmark a User")
 
-                if request.form["email"] != "":
-                    qry = qry.filter_by(email = request.form["email"])
-                if request.form["telephone"] != "":
-                    qry = qry.filter_by(phone = request.form["telephone"])
-                if request.form["ssn"] != "":
-                    qry = qry.filter_by(ssn = request.form["ssn"])
+                users, status = gateway.search_users(request.form)
             except:
                 flash("Bad Form","error")
-                return render_template('form.html', form=form, title="Unmark a User")
+                return render_template('mark_positives.html', form=form, title="Mark a User")
 
-            qry = qry.all()
+            if users is None or status != 200:
+                return make_response(render_template('error.html', error = status), status)
 
-            if len(qry) == 0:
+            if len(users) == 0:
                 flash("User not found","error")
                 return render_template('form.html', form=form, title="Unmark a User")
-            elif len(qry) > 1:
+            elif len(status) > 1:
                 flash("More users have been found, please try again","error")
                 return render_template('form.html', form=form, title="Unmark a User")
             else:
-                qry = qry[0]
+                user = users[0]
 
-            if not qry.is_positive:
+            if not user['is_positive']:
                 flash("The user is not positive","warning")
                 return render_template('form.html', form=form, title="Unmark a User")
 
-            if unmark_as_positive(qry.id):
+            result, status = gateway.unmark_user(user['id'])
+
+            if result is not None and status == 200:
                 flash("The user was unmarked","success")
                 return redirect("/positives")
             else:
@@ -201,34 +162,15 @@ def _mark_as_positive_by_id(pos_id):
     Error status codes:
         404 -- User not found
     """
-    qry = db.session.query(User)\
-        .filter_by(id = pos_id)\
-        .filter_by(is_admin = False)\
-        .filter_by(is_health_authority=False)\
-        .filter_by(rest_id = None)\
-        .first()
+    user, status = gateway.mark_user(pos_id)
 
-    if qry is None:
+    if status == 404:
         flash("User not found","error")
         return make_response(render_template('error.html', error='404'), 404)
+    elif user is None or status != 200:
+        return make_response(render_template("error.html", error = status), status)
 
-    if mark_as_positive(qry.id):
-        flash("The user was marked","success")
-        two_weeks = timedelta(days=14)
-        today = datetime.today()
-        date_start = today - two_weeks
-        users_to_be_notificated = get_user_contacts(qry.id, date_start, today)
-        for user in users_to_be_notificated:
-            add_notification(qry.id, user.id, 0)
-        operators_to_be_notified = get_operators_contacts(qry.id, date_start, today)
-        for operator in operators_to_be_notified:
-            add_notification(qry.id, operator.id, 1)
-        add_bookings_notifications(qry.id)
-        return redirect("/positives")
-    else:
-        flash("User not found","error")
-        return make_response(render_template('error.html', error='404'), 404)
-
+    return redirect("/positives")
 
 @contact_tracing.route('/positives/<int:pos_id>/unmark', methods=['GET'])
 @health_authority_required
@@ -239,28 +181,15 @@ def _unmark_as_positive_by_id(pos_id):
     Error status codes:
         404 -- User not found or user not positive
     """
-    qry = db.session.query(User)\
-        .filter_by(id = pos_id)\
-        .filter_by(is_admin = False)\
-        .filter_by(is_health_authority=False)\
-        .filter_by(rest_id = None)\
-        .first()
+    user, status = gateway.unmark_user(pos_id)
 
-    if qry is None:
+    if status == 404:
         flash("User not found","error")
         return make_response(render_template('error.html', error='404'), 404)
+    elif user is None or status != 200:
+        return make_response(render_template("error.html", error = status), status)
 
-    if not qry.is_positive:
-        flash("The user is not positive","warning")
-        return make_response(render_template('error.html', error='404'), 404)
-
-    if unmark_as_positive(qry.id):
-        flash("The user was unmarked","success")
-        return redirect("/positives")
-    else:
-        flash("User not found","error")
-        return make_response(render_template('error.html', error='404'), 404)
-
+    return redirect("/positives")
 
 @contact_tracing.route('/positives/<int:user_id>/contacts')
 @health_authority_required
@@ -270,20 +199,14 @@ def user_contacts(user_id):
     Error status codes:
         404 -- User not found or user not positive
     """
-    qry = db.session.query(User)\
-        .filter_by(id = user_id)\
-        .filter_by(is_admin = False)\
-        .filter_by(is_health_authority=False)\
-        .filter_by(rest_id = None)\
-        .first()
+    users, status = gateway.get_user_contacts(user_id,
+        datetime.today() - timedelta(days = 14),
+        datetime.today())
 
-    if qry is None:
+    if status == 404:
         flash("User not found","error")
         return make_response(render_template('error.html', error='404'), 404)
-
-    if not qry.is_positive:
-        flash("The user is not positive","warning")
-        return make_response(render_template('error.html', error='404'), 404)
+    elif status != 200:
+        return make_response(render_template('error.html', error=status), status)
         
-    return render_template("users.html",
-        users=get_user_contacts(user_id, datetime.today() - timedelta(days=14), datetime.today()))
+    return render_template("users.html", users=users)
