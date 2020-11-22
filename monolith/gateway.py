37452fe7ea99f59ce417dd407dc05ec136062454
GBisi <giuseppe.bisicchia.98@hotmail.it>
2020-11-22 20:45:19 +0100

    Added bookings gateway interface

--- monolith/gateway.py
+++ monolith/gateway.py
@@ -73,16 +73,67 @@ class RealGateway(GatewayInterface):
         return get(f"{self.addr}/bookings?user={user_id}&from={today}")
 
     #### RESERVATIONS ####
-    def get_reservation(self, reservation_id):
-        return get(f"{self.addr}/bookings/{reservation_id}")
+    def get_bookings(self, user=None, rest=None, table=None, begin=None, end=None, begin_entrance=None, end_entrance=None, with_user=True):
+        url = self.addr+"bookings?"
 
-    def delete_reservation(self, reservation_id):
-        return delete(f"{self.addr}/bookings/{reservation_id}")
+        if user is not None:
+            url += "user="+str(user)+"&"
 
-    # TODO
-    def register_entrance(self, reservation_id):
-        pass
-    
-    # TODO
-    def get_reservations(self, restaurant_id, begin, end):
-        pass
+        if rest is not None:
+            url += "rest="+str(rest)+"&"
+
+        if table is not None:
+            url += "table="+str(table)+"&"
+
+        if begin is not None:
+            url += "begin="+str(begin)+"&"
+
+        if end is not None:
+            url += "end="+str(end)+"&"
+
+        if begin_entrance is not None:
+            url += "begin_entrance="+str(begin_entrance)+"&"
+
+        if end_entrance is not None:
+            url += "end_entrance="+str(end_entrance)+"&"
+            
+        if with_user:
+            url += "with_user=true"
+
+        return get(url)
+
+    def get_a_booking(self, id, with_user=True):
+        url = self.addr+"bookings/"+str(id)
+        if with_user:
+            url += "?with_user=true"
+        return get(url)
+
+    def new_booking(self, user_id, rest_id, number_of_people, booking_datetime):
+        booking = {
+            "user_id":user_id,
+            "restaurant_id":rest_id,
+            "number_of_people":number_of_people,
+            "booking_datetime":booking_datetime,
+        }
+
+        return post(self.addr+"bookings",booking)
+
+    def edit_booking(self, booking_id, number_of_people=None, booking_datetime=None, entrance=False):
+        booking = {
+        }
+
+        if number_of_people is not None:
+            booking["number_of_people"] = number_of_people
+
+        if booking_datetime is not None:
+            booking["booking_datetime"] = booking_datetime
+
+        url = self.addr+"bookings/"+str(booking_id)
+
+        if entrance:
+            url += "?entrance=true"
+
+        return put(url,booking)
+
+    def delete_booking(self, id):
+        return delete(self.addr+"bookings/"+str(id))
