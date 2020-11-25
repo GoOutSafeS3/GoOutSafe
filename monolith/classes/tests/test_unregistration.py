1b9dc32b4a7701abc0f2a7be5c2464279403d877
mrc-g <mrc-g@users.noreply.github.com>
2020-11-25 20:51:03 +0100

    fixed unregister tests

--- monolith/classes/tests/test_unregistration.py
+++ monolith/classes/tests/test_unregistration.py
@@ -3,7 +3,7 @@ from monolith.app import create_app
 from flask_test_with_csrf import FlaskClient
 from flask import url_for
 from flask_login import current_user
-from monolith.classes.tests.utils import do_login, do_logout
+from monolith.classes.tests.utils import do_login, do_logout, send_registration_form
 
 class TestUnregistration(unittest.TestCase):
     @classmethod
@@ -43,13 +43,46 @@ class TestUnregistration(unittest.TestCase):
 
     def test_delete_log_as_operator(self):
         client = self.setup_app()        
-        do_login(client, "operator@example.com","operator")
+        form = {
+            "email":"delete-operator@test.me",
+            "firstname":"Operator",
+            "lastname":"delete",
+            "password":"operator",
+            "password_repeat":"operator",
+            "dateofbirth":"01/01/1970",
+            "telephone":"12345671234",
+            "ssn": "",
+        }
+
+        rest = {
+            "name":"deleted restaurant",
+            "phone":"12345671234",
+            "lat":"43.431489",
+            "lon":"10.242911",
+            "first_opening_hour": "13",
+            "first_closing_hour": "15",
+            "second_opening_hour": "19",
+            "second_closing_hour": "23",
+            "occupation_time": "2",
+            "closed_days": ["1","2"],
+            "cuisine_type": "Pizzoria",
+            "menu": "Napolitano"
+        }
+        reply = send_registration_form(client, '/create_operator', form)
+        self.assertEqual(
+            reply["status_code"],
+            302, msg = reply["html"])
+        reply = client.t_get('/')
+        reply = send_registration_form(client, '/create_restaurant', rest)
+        self.assertEqual(
+            reply["status_code"],
+            302, msg = reply["html"])
 
         reply = client.t_get('/delete')
         self.assertEqual(reply.status_code, 200, msg=reply.get_data(as_text=True))
 
         form = {
-            'email': "operator@example.com",
+            'email': "delete-operator@test.me",
             'password': "operator"
         }
         reply = client.t_post('/delete', data=form)
@@ -58,27 +91,44 @@ class TestUnregistration(unittest.TestCase):
         do_login(client, "admin@example.com", "admin")
         
         reply = client.t_get('/restaurants')
-        self.assertNotIn("Trial Restaurant", reply.get_data(as_text=True), msg=reply.get_data(as_text=True))
+        self.assertNotIn("deleted restaurant", reply.get_data(as_text=True), msg=reply.get_data(as_text=True))
 
         reply = client.t_get('/users')
-        self.assertNotIn("Operator Operator", reply.get_data(as_text=True), msg=reply.get_data(as_text=True))
+        self.assertNotIn("Operator delete", reply.get_data(as_text=True), msg=reply.get_data(as_text=True))
         
         do_logout(client)
 
 
-        reply = do_login(client, "operator@example.com","operator")
+        reply = do_login(client, "delete-operator@test.me","operator")
         self.assertEqual(reply.status_code, 401)
 
     def test_delete_log_as_customer(self):
-        client = self.setup_app()        
-        do_login(client, "gianni@example.com","gianni")
+        client = self.setup_app()
+        
+        form = {
+            "email":"delete-user@test.me",
+            "firstname":"asjdn",
+            "lastname":"asjdn",
+            "password":"42",
+            "password_repeat":"42",
+            "dateofbirth":"01/01/1970",
+            "telephone":"01234567890",
+            "ssn":""
+        }
+
+        reply = send_registration_form(client, '/create_user', form)
+        
+        self.assertEqual(
+            reply["status_code"],
+            302,msg=reply)        
+        do_login(client, "delete-user@test.me","42")
 
         reply = client.t_get('/delete')
         self.assertEqual(reply.status_code, 200, msg=reply.get_data(as_text=True))
 
         form = {
-            "email": "gianni@example.com",
-            "password": "gianni"
+            "email": form["email"],
+            "password": form["password"]
         }
         reply = client.t_post('/delete', data=form)
         self.assertEqual(reply.status_code, 302, msg=reply.get_data(as_text=True))
@@ -87,10 +137,10 @@ class TestUnregistration(unittest.TestCase):
 
         do_login(client, "admin@example.com", "admin")
         reply = client.t_get('/users')
-        self.assertNotIn("Customer Customer", reply.get_data(as_text=True), msg=reply.get_data(as_text=True))
+        self.assertNotIn("asjdn asjdn", reply.get_data(as_text=True), msg=reply.get_data(as_text=True))
         do_logout(client)
 
-        reply = do_login(client, "gianni@example.com","gianni")
+        reply = do_login(client, "delete-user@test.me","42")
         self.assertEqual(reply.status_code, 401)
 
 
@@ -99,8 +149,7 @@ class TestUnregistration(unittest.TestCase):
         do_login(client, "alice@example.com","alice")
 
         reply = self.do_delete(client, "alice@example.com","alice")
-        self.assertEqual(reply.status_code, 302, msg=reply.get_data(as_text=True))
-        reply = client.t_get('/')
+        self.assertEqual(reply.status_code, 400, msg=reply.get_data(as_text=True))
         self.assertIn("You cannot delete your data as long as you are positive",reply.get_data(as_text=True),  msg=reply.get_data(as_text=True))
 
 
@@ -113,8 +162,27 @@ class TestUnregistration(unittest.TestCase):
 
     def test_delete_post_wrong_mail(self):
         client = self.setup_app()        
-        do_login(client, "gianni@example.com","gianni")
-        reply = self.do_delete(client, "the_customer@example.com","customer")
+        form = {
+            "email":"delete-user@test.me",
+            "firstname":"asjdn",
+            "lastname":"asjdn",
+            "password":"42",
+            "password_repeat":"42",
+            "dateofbirth":"01/01/1970",
+            "telephone":"01234567890",
+            "ssn":""
+        }
+
+        reply = send_registration_form(client, '/create_user', form)
+        
+        self.assertEqual(
+            reply["status_code"],
+            302,msg=reply)        
+        do_login(client, "delete-user@test.me","42")
+
+        reply = client.t_get('/delete')
+        self.assertEqual(reply.status_code, 200, msg=reply.get_data(as_text=True))
+        reply = self.do_delete(client, "the_customer@example.com","42")
         self.assertEqual(reply.status_code, 400, msg=reply.get_data(as_text=True))
         do_logout(client)
 
